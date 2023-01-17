30d59e6e93b112c4c95cdecf23d7f0cb06e3116b
bank app <saurabhsngh5484@gmail.com>
2023-01-17 23:42:45 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    static class A{
+        public String meth()
+        {
+            return "Invoking method from class A";
+
+        }
+    }
+    static class B extends A{
+        @java.lang.Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args) {
+        B object =new B();
+        object.meth();
+        B obj = new B();
+        obj.meth();
+
+    }
 }
